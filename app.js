c21189bfb25bba6d2360a7071dc8059f9ef4a5a8
dhanushkrock@gmail.com <dhanushkrock@gmail.com>
2025-03-20 15:05:00 +0530

    env added

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 // Use environment variables for sensitive information
 const PORT = process.env.PORT || 3000;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://dhanushkrock:Hb76UwwZTsFNXOWP@cluster0.gbp6t.mongodb.net/test_db?retryWrites=true&w=majority";
+const MONGODB_URI = process.env.MONGODB_URI;
 
 // Import middleware and routes
 /* const authenticationMiddleware = require('./middleware/authentication'); */
-const imageRoutes = require('./routes/image');
-const userRoutes = require('./routes/user');
-const adminRoutes = require('./routes/admin');
-const bookRoutes = require('./routes/book');
-const selectedPlaceRoutes = require('./routes/selectedPlace');
-const paymentRoutes = require('./routes/payment');
+const imageRoutes = require("./routes/image");
+const userRoutes = require("./routes/user");
+const adminRoutes = require("./routes/admin");
+const bookRoutes = require("./routes/book");
+const selectedPlaceRoutes = require("./routes/selectedPlace");
+const paymentRoutes = require("./routes/payment");
 
 // Use middleware
 app.use(bodyParser.json());
@@ -23,15 +26,24 @@ app.use(cors());
 /* app.use(authenticationMiddleware); */
 
 // Use routes
-app.use('/image', imageRoutes);
-app.use('/user', userRoutes);
-app.use('/book', bookRoutes);
-app.use('/api/visits', selectedPlaceRoutes);
-app.use('/pay', paymentRoutes);
-app.use('/admin', adminRoutes); // Use adminRoutes here
+app.use("/image", imageRoutes);
+app.use("/user", userRoutes);
+app.use("/book", bookRoutes);
+app.use("/api/visits", selectedPlaceRoutes);
+app.use("/pay", paymentRoutes);
+app.use("/admin", adminRoutes); // Use adminRoutes here
 
 // Connect to MongoDB
-mongoose.connect(MONGODB_URI, { /* useNewUrlParser: true, useUnifiedTopology: true  */});
+mongoose
+  .connect(MONGODB_URI, {
+    /* useNewUrlParser: true, useUnifiedTopology: true  */
+  })
+  .then(() => {
+    console.log("DB Connected Success");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // Start the server
 app.listen(PORT, () => {
